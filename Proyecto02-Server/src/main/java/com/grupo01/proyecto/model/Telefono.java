c424e8e422a67dd6ba6368651868992cd1e34778
Santi <santialovera20@gmail.com>
2019-05-23 09:43:56 +0200

    Correcion JsonIgnoreType

--- Proyecto02-Server/src/main/java/com/grupo01/proyecto/model/Telefono.java
+++ Proyecto02-Server/src/main/java/com/grupo01/proyecto/model/Telefono.java
@@ -19,7 +19,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreType;
  */
 @Entity
 @NamedQuery(name = "Telefono.findAll", query = "SELECT t FROM Telefono t")
-@JsonIgnoreType
 public class Telefono implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -33,7 +32,6 @@ public class Telefono implements Serializable {
 	// bi-directional many-to-one association to Persona
 	@ManyToOne
 	@JoinColumn(name = "idpersona")
-	@JsonIgnore
 	private Persona persona;
 
 	public Telefono() {
